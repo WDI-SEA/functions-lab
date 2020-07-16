84d3884d4aa8c734e1a2cbf9a693966c8b970dcf
SUBRATA ROY <subrata.r321@gmail.com>
2020-07-15 23:55:38 -0400

    [Solution] add for function homework

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,47 +1,115 @@
 // Question 1
-function maxOfTwoNumbers() {
+function maxOfTwoNumbers(one,two) {
+    if (one>two) {
+        console.log("The largest number is " +one);
+    } else {
+        console.log("The largest number is " +two);
+
+    }
 
 }
+// function called
+maxOfTwoNumbers(200,55);
 
 // Question 2
-function maxOfThree() {
+function maxOfThree(one,two,three) {
+    if (one>two && one>three) {
+        console.log("The largest number is " +one);
+    } else if (two>one && two>three){
+        console.log("The largest number is " +two);
+    } else {
+        console.log("The largest number is " +three);
+    }
 
 }
+// function called
+maxOfThree(20,50,85);
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(char) {
+    if (char === "A" || char === "E" || char === "I" || char === "O" || char === "U") {
+        console.log("True");
+    } else if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+        console.log("True");
+    } else {
+        console.log("Flase");
+    }
+
 
 }
+// function called
+isCharacterAVowel("u");
 
 // Question 4
-function sumArray() {
+function sumArray(array) {
+    let result = 0;
+    for (let i = 0 ; i <array.length ; i++) {
+        result += array[i];
+
+    }
+    return result;
  
 }
-
+// calls the function and print the result in console
+console.log(sumArray([1,2,9,4]));
 
 // Question 4
-function multiplyArray() {
+function multiplyArray(array) {
+    let result = 1;
+    for (let i = 0 ; i <array.length ; i++) {
+        result = result * array[i];
+
+    }
+    return result;
 
 }
+// calls the function and print the result in console
+console.log(multiplyArray([1,2,7,5]));
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+    argumentsArray = Array.from(arguments);
+    console.log(argumentsArray.length);
 }
+
+// calls the function and print the result in console
+numberOfArguments(2,9,5,6,8);
 
 
 
 // Question 6
-var reverseString = function (){
-  
-};
+var reverseString = function (str){
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) { 
+        newString += str[i];
+}
+return newString};
+// calls the function and print the result in console
+console.log(reverseString("hello"));
 
 
 // Question 7
-function findLongestWord () {
-  
+
+// var word= "";
+function findLongestWord (array) {
+    var longestWord= 0;
+    for(let i=0; i<array.length; i++){
+        if (array[i].length > longestWord) {
+            longestWord = array[i].length;
+        }
+        
+    }
+//    word= array[i];
+    return longestWord;
+    
 }
+
+// calls the function and print the result in console
+console.log(findLongestWord(['David', 'Subrata', 'Martin', 'Yoel', 'han']));
+// console.log("The longest word is " +array[i]);
+
+
 
 
 // Question 8
