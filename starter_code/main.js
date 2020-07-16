ab3c4611c63a21cf1f35db6fd7b0b9368c600fb9
Solomon Abraha <solomon@Solomons-MacBook-Pro.local>
2020-07-16 09:28:29 -0600

    added soulutions

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,53 +1,97 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function getArrayMax(array) {
+    return Math.max.apply(null,array);
 }
+var maxOfTwoNumbers=[35, 56];
+var maxOfTwoNumbers=getArrayMax(maxOfTwoNumbers)
+maxOfTwoNumbers
 
 // Question 2
-function maxOfThree() {
-
+function getArrayMax(array) {
+    return Math.max.apply(null,array);
 }
+var maxOfThree=[35, 56, 82];
+var maxOfThree=getArrayMax(maxOfThree)
+maxOfThree
+
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(x) {  return /[aeiouAEIOU]/.test(x); }
 
+var input = '';
+while (input.length != 1) {
+  input = prompt("Enter a character ");
 }
+alert(isCharacterAVowel(input));
 
 // Question 4
-function sumArray() {
- 
-}
+function sumArray(array){
+    var total = 0,
+        len = array.length;
+  
+    for (var i = 0; i < len; i++){
+      total += array[i];
+    }
+  
+    return total;
+  };
+  
+  var my_array = [1,2,3,4];
+  
+  console.log( sumArray( my_array ) );
 
 
 // Question 4
-function multiplyArray() {
-
+function multiply (array) {
+    var sum=1;
+    for (var i=0; i<array.length; i++) {
+        sum = sum * array[i];
+    } 
+    return sum;
 }
+console.log(multiply[1,2,3]);
 
 
 // Question 5
-var numberOfArguments = function(){
-  
+var numberOfArguments = 
+function print_args() {
+    for(var i=0; i<arguments.length; i++)
+        console.log(arguments[i])
 }
 
 
 
 // Question 6
-var reverseString = function (){
-  
-};
-
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('reversed string');
 
 // Question 7
-function findLongestWord () {
+function findLongestWord(...strs) {
+    return strs.sort(function(a, b) {return b.length - a.length})[0];
+  }
   
-}
+  console.log(findLongestWord('john', 'johnsmith', 'solomonabraha'));
 
 
 // Question 8
-function filterLongWords () {
-  
-}
+var filterLongWords = 
+
+function(array, int){
+    var length = array.length;
+    var longestWords = [];
+    for (i = 0; i < length; i++) {
+      if (array[i].length > int) {
+        longestWords[longestWords.length] = array[i];
+      }
+    }
+    return longestWords;
+  }
 
 
 // Bonus 1
@@ -55,7 +99,12 @@ function filterLongWords () {
 
 
 // Bonus 2
-function charactersOccurencesCount() {
-  
+function charactersOccurencesCount(string) {
+    var freq = {};
+    for (var i = string.length - 1; i >= 0; i--) {
+        freq[string[i]] = 1 + (freq[string[i]] || 0);
+    }    
+    return freq;
 }
+console.log(charactersOccurencesCount('dlsdlkfjskkjjdddfjfjduter'))
 
